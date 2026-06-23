b1f6a3de35e21eea80f76e917144f8c1e028e904
Alexis Barrett <happynessb09@gmail.com>
2026-06-23 13:31:26 -0400

    forgot to push. currently working on the shopping cart

--- src/main/java/org/yearup/service/ShoppingCartService.java
+++ src/main/java/org/yearup/service/ShoppingCartService.java
@@ -1,8 +1,15 @@
 package org.yearup.service;
 
 import org.springframework.stereotype.Service;
+import org.yearup.models.CartItem;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.repository.ShoppingCartRepository;
+
+import java.util.List;
+
+//import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
 
 @Service
 public class ShoppingCartService
@@ -20,8 +27,28 @@ public class ShoppingCartService
     public ShoppingCart getByUserId(int userId)
     {
         // load the user's cart rows, look up each product, and build the ShoppingCart
-        return null;
+        List<CartItem> cartItems = shoppingCartRepository.findByUserId(userId); //Gets the rows
+        ShoppingCart cart = new ShoppingCart();
+
+        for (CartItem item: cartItems){
+            Product product = productService.getById(item.getProductId());
+
+            ShoppingCartItem cartItem = new ShoppingCartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(item.getQuantity());
+
+            cart.add(cartItem);
+        }
+        return cart;
     }
 
     // add additional methods here
+    public CartItem getCartItem(int userId, int productId) {
+        return shoppingCartRepository.findByUserIdAndProductId(userId,productId);
+    }
+
+    public void clearCart(int userId){
+        shoppingCartRepository.deleteByUserId(userId);
+    }
+
 }
